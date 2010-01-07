ef2aae47f5e679ee63010dc19772320871f54a91
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2010-01-07 11:35:01 +0100

    Dev pull updates

--- dev-pull-bin/setup-remote.sh
+++ dev-pull-bin/setup-remote.sh
@@ -1,9 +1,9 @@
 
 if [ ! -d $REMOTE_GIT_DIR ]; then
   mkdir -p $REMOTE_GIT_DIR
+fi
+
+if [ ! -d $REMOTE_GIT_DIR/.git ]; then
   cd $REMOTE_GIT_DIR
   git init
-  mysqldump -h $REMOTE_HOST -P $REMOTE_PORT -u $REMOTE_USER -p"$REMOTE_PASS" $REMOTE_DB > dump.sql
-  git add dump.sql
-  git commit -m "Initial dump added `date +"%Y-%m-%d %H:%M:%S"`"
 fi
